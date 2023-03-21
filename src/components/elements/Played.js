e9c300a6caf1a39668b9ea1f81170ea26500732f
NRO1 <nrdevac1@gmail.com>
2023-03-21 17:35:37 +0200

    added elements1

--- src/components/elements/Played.js
+++ src/components/elements/Played.js
@@ -1,15 +1,35 @@
 import classes from "./Played.module.css";
+import { PieChart, Pie } from 'recharts';
  
 
 function Played(props) {
-
-
-
+  console.log(props.data)
+  const data_wins = [
+    {name: 'wins-away', value: props.data[1].away},
+    {name: 'wins-home', value: props.data[1].home},
+  ]
+      
   return (
     <div>
         <p className={classes.header}>משחקים מתחילת העונה</p>
-        <div>
-    
+      <div>
+        <div className={classes.container}>
+            <p>משחקים ששוחקו</p>
+              <PieChart width={400} height={400}>
+                <Pie
+                  dataKey="value"
+                  startAngle={180}
+                  endAngle={0}
+                  data={data_wins}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#D50BD9"
+                  label
+            />
+          </PieChart>
+          <p>בית  -  חוץ</p>
+        </div>
       </div>
     </div>
   );
