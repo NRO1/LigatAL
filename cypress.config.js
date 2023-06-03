307bbb73c890fe385760753653c8eb977d6268cd
NRO1 <nrdevac1@gmail.com>
2023-06-03 21:30:05 +0300

    adding E2E

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "https://ec2-3-69-9-45.eu-central-1.compute.amazonaws.com",
-    screenshotOnRunFailure: false,
-    video: false
-  },
-});
--- cypress/e2e/ligat-al.cy.js
+++ cypress/e2e/ligat-al.cy.js
@@ -2,11 +2,9 @@
 
 
 describe('Ligat-al web testing', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
 
   it('should have title', () => {
+    cy.visit("https://ec2-3-69-9-45.eu-central-1.compute.amazonaws.com")
     cy.wait(200)
     cy.get('#main_title').contains("ליגת העל")
   })
