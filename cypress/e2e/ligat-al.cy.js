465a571978503a82f53dadf4475d369ca3570e86
NRO1 <nrdevac1@gmail.com>
2023-06-03 22:30:00 +0300

    cypress

--- cypress/e2e/ligat-al.cy.js
+++ cypress/e2e/ligat-al.cy.js
@@ -2,10 +2,14 @@
 
 
 describe('Ligat-al web testing', () => {
-  it('should have title', () => {
+  it('should have main DIV', () => {
     cy.visit("/")
-    cy.wait(200)
+    cy.wait(400)
     cy.get('div').should('have.class',"App")
+  })
+
+  it('should have group selection', () => {
+    cy.visit("/")
     cy.get('div').should('have.class',"Nav_g_container__+qeC3").should("have.length", 12)
   })
 })
