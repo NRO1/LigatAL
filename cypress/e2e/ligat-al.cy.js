b823500c4e28def9a3a686e9d9e335a888f44628
NRO1 <nrdevac1@gmail.com>
2023-06-03 22:23:24 +0300

    cypress

--- cypress/e2e/ligat-al.cy.js
+++ cypress/e2e/ligat-al.cy.js
@@ -5,7 +5,7 @@ describe('Ligat-al web testing', () => {
   it('should have title', () => {
     cy.visit("/")
     cy.wait(200)
-    cy.get('[data-cy="main_title"]').contains("ליגת העל")
-    cy.get('[data-cy="list"]').should("have.length", 12)
+    cy.get('div').should('have.class',"App")
+    cy.get('div').should('have.class',"Nav_g_container__+qeC3").should("have.length", 12)
   })
 })
