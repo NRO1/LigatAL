79407b31ba2b0836357f7070342e8c8cf1712fc0
NRO1 <nrdevac1@gmail.com>
2023-06-03 18:35:06 +0300

    adding E2E testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "https://ec2-3-69-9-45.eu-central-1.compute.amazonaws.com",
     screenshotOnRunFailure: false,
     video: false
